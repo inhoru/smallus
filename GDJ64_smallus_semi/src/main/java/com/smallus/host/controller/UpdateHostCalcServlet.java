523f42e86f31182c57f197699def89cd11c54270
GDJ10 <GDJ10@DESKTOP-RO316NV>
2023-06-21 14:23:55 +0900

    호스트 헤더 수정

--- GDJ64_smallus_semi/src/main/java/com/smallus/host/controller/UpdateHostCalcServlet.java
+++ GDJ64_smallus_semi/src/main/java/com/smallus/host/controller/UpdateHostCalcServlet.java
@@ -34,22 +34,13 @@ public class UpdateHostCalcServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		String hostId=request.getParameter("hostId");
+		System.out.println(hostId);
 		Host host= new HostService().updateHostCalc(hostId);
 		
-		// Gson 라이브러리 이용해서 json parsing하기
-		// 1. Gson 클래스 생성하기
 		Gson gson= new Gson();
-		// 2. Gson에서 제공하는 parsing하는 메소드 사용하기
-		// toJson(json으로 변경할 객체[,OutputStream]); // outputStream으로 변경한 객체 보내고 끝난다
 		response.setContentType("application/json; charset=UTF-8");
-		//gson.toJson(m,response.getWriter()); // 첫번째 멤버변수를 key값으로 쓴다.
-		gson.toJson(host,response.getWriter()); // list 보낼 때는 바로 list 보내도 됨 ㅠㅠ
+		gson.toJson(host,response.getWriter()); 
 		
-		// json 형태로 전송된 데이터를 vo 객체로 만들어준다.
-		//gson.fromJson(String, null)
-		String data=request.getParameter("data");
-		Member requestData=gson.fromJson(data,Member.class); // data를 Member class로 변환해준다
-		System.out.println(requestData);
 	}
 
 	/**
