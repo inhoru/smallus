7084135779d28a97457b1e72dbea01fa368eb338
kkangmingi <alsrlwpdhkd@gmail.com>
2023-06-27 20:58:42 +0900

    2023.06.27 커밋

--- GDJ64_smallus_semi/src/main/java/com/smallus/admin/controller/InquiryEnrollServlet.java
+++ GDJ64_smallus_semi/src/main/java/com/smallus/admin/controller/InquiryEnrollServlet.java
@@ -1,11 +1,14 @@
 package com.smallus.admin.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.smallus.admin.service.AdminService;
 
 /**
  * Servlet implementation class InquiryEnrollServlet
@@ -26,8 +29,23 @@ public class InquiryEnrollServlet extends HttpServlet {
     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
     */
    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-      // TODO Auto-generated method stub
-      response.getWriter().append("Served at: ").append(request.getContextPath());
+		String boardId=request.getParameter("boardId");
+		String commentConent=request.getParameter("commentConent");
+		System.out.println(boardId+" "+commentConent);
+//		int result=new AdminService().inquiryAnswerEnroll(boardId,commentConent);
+//		String boardCheck="답변완료";
+//		int result2=new AdminService().inquiryUpdate(boardId,boardCheck);
+//		String msg,loc;
+//		if(result>0) {
+//			msg="1:1문의 답변완료 했습니다.";
+//			loc="/admin/InquiryListServlet.do";
+//		}else {
+//			msg="1:1문의 답변실패 했습니다.";
+//			loc="/admin/InquiryListServlet.do";
+//		}
+//		request.setAttribute("msg", msg);
+//		request.setAttribute("loc", loc);
+//		request.getRequestDispatcher("/views/common/msg.jsp").forward(request, response);	
    }
 
    /**
