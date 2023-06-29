291165aac6e576d8d881021143446e4f6d942eec
kkangmingi <alsrlwpdhkd@gmail.com>
2023-06-29 11:36:18 +0900

    클래스 상세페이징 처리 수정

--- GDJ64_smallus_semi/src/main/java/com/smallus/classes/controller/ViewCategoryCookingServlet.java
+++ GDJ64_smallus_semi/src/main/java/com/smallus/classes/controller/ViewCategoryCookingServlet.java
@@ -61,7 +61,7 @@ public class ViewCategoryCookingServlet extends HttpServlet {
 			if (pageNo == cPage) {
 				pageBar += "<span class='h-pageBar-now'> " + pageNo + " </span>";
 			} else {
-				pageBar += "<a href='" + request.getRequestURI() + "?cPage=" + pageNo + "&numPerpage=" + numPerpage +"'> " + pageNo + " </a>";
+				pageBar += "<a href='" + request.getRequestURI() + "?categoryId="+categoryId+"&cPage=" + pageNo + "&numPerpage=" + numPerpage +"'> " + pageNo + " </a>";
 			}
 			pageNo++;
 		}
